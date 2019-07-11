832e918412d04de7fd318333e59b5dc4c7263fe1
김정국 <pipe0502@gmail.com>
2019-07-11 14:17:05 +0900

    change env variables

--- order-service/src/main/java/com/bwg/orderservice/config/SwaggerConfig.java
+++ order-service/src/main/java/com/bwg/orderservice/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.bwg.orderservice.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -10,32 +13,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket apiDocket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.progressivecoder.ordermanagement"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(getApiInfo());
-    }
+  @Bean
+  public Docket apiDocket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(getApiInfo());
+  }
 
-    private ApiInfo getApiInfo(){
-        return new ApiInfo(
-                "Saga Pattern Implementation using Axon and Spring Boot",
-                "App to demonstrate Saga Pattern using Axon and Spring Boot",
-                "1.0.0",
-                "Terms of Service",
-                new Contact("Saurabh Dashora", "progressivecoder.com", "coder.progressive@gmail.com"),
-                "",
-                "",
-                Collections.emptyList());
-    }
+  private ApiInfo getApiInfo() {
+    return new ApiInfo(
+        "Saga Pattern",
+        "Spring-Boot with Axon",
+        "1.0.0",
+        "",
+        new Contact("Smart Kuk", "", ""),
+        "",
+        "",
+        Collections.emptyList());
+  }
 
 }
--- order-service/src/main/java/com/bwg/orderservice/controllers/OrderCommandController.java
+++ order-service/src/main/java/com/bwg/orderservice/controllers/OrderCommandController.java
@@ -1,29 +1,33 @@
 package com.bwg.orderservice.controllers;
 
-import com.bwg.orderservice.commands.OrderCreateDTO;
-import com.bwg.orderservice.services.commands.OrderCommandService;
+import java.util.concurrent.CompletableFuture;
 
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.CompletableFuture;
+import com.bwg.orderservice.commands.OrderCreateDTO;
+import com.bwg.orderservice.services.commands.OrderCommandService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/api/orders")
-@Api(value = "Order Commands", description = "Order Commands Related Endpoints", tags = "Order Commands")
+@Api(value = "Order Commands", description = "Order Commands Related Endpoints",
+    tags = "Order Commands")
 public class OrderCommandController {
 
-    private OrderCommandService orderCommandService;
+  private OrderCommandService orderCommandService;
 
-    public OrderCommandController(OrderCommandService orderCommandService) {
-        this.orderCommandService = orderCommandService;
-    }
+  public OrderCommandController(OrderCommandService orderCommandService) {
+    this.orderCommandService = orderCommandService;
+  }
 
-    @PostMapping
-    public CompletableFuture<String> createOrder(@RequestBody OrderCreateDTO orderCreateDTO){
-        return orderCommandService.createOrder(orderCreateDTO);
-    }
+  @ApiOperation(value = "The Endpoint that start saga pattern is API.")
+  @PostMapping
+  public CompletableFuture<String> createOrder(@RequestBody OrderCreateDTO orderCreateDTO) {
+    return orderCommandService.createOrder(orderCreateDTO);
+  }
 }
